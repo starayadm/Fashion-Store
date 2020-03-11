3378d1b1128ea4481cd8c713c657f3afdbd86ea2
Lily Kustova <kustova-la@mail.ru>
2020-03-11 15:19:15 +0300

    Header layout is done

---
+++ app/js/app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const headerSearchBtn = document.querySelector('.header-search__btn');
+  const headerSearchInput = document.querySelector('.header-search__input');
+});
